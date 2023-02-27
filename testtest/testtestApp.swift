03d9d6f54153cfdbe3f96ae506d9b8ee8b03a038
Your Name <you@example.com>
2023-02-27 14:59:49 +0300

    Initial Commit

---
+++ testtest/testtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  testtestApp.swift
+//  testtest
+//
+//  Created by uniteam IT on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct testtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
